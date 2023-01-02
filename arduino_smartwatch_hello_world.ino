3a905533c7d2fd04d4eb197c92b1fd877aeb0768
Leo Junquera <leo.junquera@live.com>
2023-01-02 12:13:55 -0500

    added config

--- arduino_smartwatch_hello_world.ino
+++ arduino_smartwatch_hello_world.ino
@@ -1,9 +1,32 @@
+
+#include "hardware_config.h"
+
+volatile bool inputoutput_inited = false;
+
 void setup() {
-  // put your setup code here, to run once:
+  // setup code
+  delay(500);
+  if (hardware_button_state()) {//if button is pressed on startup goto Bootloader
+    NRF_POWER->GPREGRET = BOOTLOADER_ADDRESS;
+    NVIC_SystemReset();
+  }
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+
+bool hardware_button_state() {
+  //get the state of the button
+  if (!inputoutput_inited)init_inputoutput();
+  if (PUSH_BUTTON_OUT != -1) {
+    pinMode(PUSH_BUTTON_OUT, OUTPUT);
+    digitalWrite(PUSH_BUTTON_OUT, HIGH);
+  }
+  bool button_state = digitalRead(PUSH_BUTTON_IN);
+  return button_state;
 }
