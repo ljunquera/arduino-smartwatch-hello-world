2be7464352c2b1fbc47b2e921bcc777a3d5e597c
Leo Junquera <leo.junquera@live.com>
2023-01-16 10:27:52 -0500

    collapsed to one file

--- hello-world/hardware_config.h
+++
@@ -1,9 +0,0 @@
-
-/* Configuration for p22 (p8 varient) */
-#define PUSH_BUTTON_IN      17
-#define PUSH_BUTTON_OUT     -1
-
-#define BOOTLOADER_ADDRESS       0x01
-
-#define STATUS_LED        27
-#define VIBRATOR_OUT      16
--- hello-world/hello-world.ino
+++ hello-world/hello-world.ino
@@ -1,21 +1,18 @@
 
-#include "hardware_config.h"
-#include <lvgl.h>
+#define PUSH_BUTTON_IN      17
+#define PUSH_BUTTON_OUT     -1
+
+#define BOOTLOADER_ADDRESS       0x01
+
+#define STATUS_LED        27
+#define VIBRATOR_OUT      16
 
 #define LF_FREQUENCY 32768UL
 #define SECONDS(x) ((uint32_t)((LF_FREQUENCY * x) + 0.5))
 #define wakeUpSeconds 0.040
 
-
-
-
-/*Change to your screen resolution*/
-static const uint16_t screenWidth  = 240;
-static const uint16_t screenHeight = 240;
-
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[ screenWidth * 10 ];
-
+const int BLINKING_LIMIT = 5;
+int blinkCount = 0;
 
 
 void setup() {
@@ -26,7 +23,6 @@ void setup() {
 
   delay(5000);
    
-  //get the state of the button
   if (!inputoutput_inited) {
     
       pinMode(STATUS_LED, OUTPUT);
@@ -38,8 +34,8 @@ void setup() {
         digitalWrite(PUSH_BUTTON_OUT, HIGH);
       }
     
-      set_led(0);
-      set_motor(0);
+      digitalWrite(STATUS_LED, LOW);
+      digitalWrite(VIBRATOR_OUT, HIGH);
       inputoutput_inited = true;
   }
 
@@ -50,32 +46,34 @@ void setup() {
   }
   
 
-  if (button_state) {//if button is pressed on startup goto Bootloader
-    reset_to_bootloader();
+  if (button_state) {
+    NRF_POWER->GPREGRET = BOOTLOADER_ADDRESS;
+    NVIC_SystemReset();
   }
   
-  //rtc_init();
-  //lv_init();
-  //display_label();
+  NVIC_ClearPendingIRQ(RTC2_IRQn);
+  NVIC_SetPriority(RTC2_IRQn, 15);
+  NVIC_EnableIRQ(RTC2_IRQn);
+
+  NRF_RTC2->PRESCALER = 0;
+  NRF_RTC2->CC[0] = SECONDS(wakeUpSeconds);
+  NRF_RTC2->INTENSET = RTC_EVTENSET_COMPARE0_Enabled << RTC_EVTENSET_COMPARE0_Pos;
+  NRF_RTC2->EVTENSET = RTC_INTENSET_COMPARE0_Enabled << RTC_INTENSET_COMPARE0_Pos;
+  NRF_RTC2->TASKS_START = 1;
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //lv_timer_handler(); /* let the GUI do its work */ 
-  delay( 100000 );
-  reset_to_bootloader();
-  
+
+    blinkCount++;
+    if (blinkCount < BLINKING_LIMIT) {
+        digitalWrite(VIBRATOR_OUT, HIGH);
+        delay(500);
+        digitalWrite(VIBRATOR_OUT, LOW);
+    }
+    delay(500);
 
 }
-
-void reset_to_bootloader() {
-    NRF_POWER->GPREGRET = BOOTLOADER_ADDRESS;
-    NVIC_SystemReset();
-}
-
-
-
 
 void set_led(bool state) {
   if (state)
@@ -89,28 +87,4 @@ void set_motor(bool state) {
     digitalWrite(VIBRATOR_OUT, LOW);
   else
     digitalWrite(VIBRATOR_OUT, HIGH);
-}
-
-void display_label() {
-
-    String LVGL_Arduino = "Hello Arduino! ";
-    LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
-  
-      /* Create simple label */
-    lv_obj_t *label = lv_label_create( lv_scr_act() );
-    lv_label_set_text( label, LVGL_Arduino.c_str() );
-    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
-}
-
-void rtc_init() {
-
-  NVIC_ClearPendingIRQ(RTC2_IRQn);
-  NVIC_SetPriority(RTC2_IRQn, 15);
-  NVIC_EnableIRQ(RTC2_IRQn);
-
-  NRF_RTC2->PRESCALER = 0;
-  NRF_RTC2->CC[0] = SECONDS(wakeUpSeconds);
-  NRF_RTC2->INTENSET = RTC_EVTENSET_COMPARE0_Enabled << RTC_EVTENSET_COMPARE0_Pos;
-  NRF_RTC2->EVTENSET = RTC_INTENSET_COMPARE0_Enabled << RTC_INTENSET_COMPARE0_Pos;
-  NRF_RTC2->TASKS_START = 1;
 }
